99647c8cbb7e11922ad2f08631bc4bec783be5a1
RifzPz <120087346+RifzPz@users.noreply.github.com>
2023-02-23 13:29:33 +0700

    Update showlogo.dart
    edit 23/02/2023

--- lib/showlogo.dart
+++ lib/showlogo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hex_color/flutter_hex_color.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sigma_space/login.dart';
+import 'package:sigma_space/main.dart';
 
 class showlogo extends StatefulWidget {
   showlogo({Key? key}) : super(key: key);
@@ -10,13 +12,26 @@ class showlogo extends StatefulWidget {
 }
 
 class _showlogoState extends State<showlogo> {
+  List<String>? strintpre;
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (context) => login()));
+    autologin();
+    Future.delayed(const Duration(seconds: 2), () async {
+      if (strintpre!.isEmpty) {
+        Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: (context) => login()));
+      } else {
+        Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: (context) => MyApp()));
+      }
     });
+
     super.initState();
+  }
+
+  Future autologin() async {
+    final preferences1 = await SharedPreferences.getInstance();
+    strintpre = await preferences1.getStringList("codestore")!;
   }
 
   @override
